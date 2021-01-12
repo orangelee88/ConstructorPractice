f2d80df825881f74b0f63f6cc8f53d763171d3a4
OrangeLee <a0487387521@gmail.com>
2021-01-12 14:37:50 +0800

    ConstructorPractice

---
+++ src/ConstructorPractice.java
@@ -0,0 +1,31 @@
+
+public class ConstructorPractice {
+
+	public static void main(String[] args) {
+		Test people = new Test("george", 28);// 宣告與新增物件
+		//people.mathod();// 執行物件的方法
+	}
+}
+
+ class Test {
+	
+		// 成員變數
+		public String Name;
+		public int Age;
+
+		public Test(String name) {
+			this.Name = name;
+		}
+
+		public Test(String name, int age) {
+			this.Name = name;
+			this.Age = age;
+			//System.out.println("姓 名 =" + Name + " ,年 紀 =" + Age);
+		}
+
+		// 方法
+//		public void mathod() {
+//			System.out.println("姓 名 =" + Name + " ,年 紀 =" + Age);
+//		}
+	}
+
